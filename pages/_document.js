ca3e47be01d1878f775683769d740e439df6e995
philip-king <philip.king@steeldynamics.com>
2022-10-19 11:47:58 -0500

    Stripped extraneous content from template, adding links to other accounts, set up email forwarding

--- pages/_document.js
+++ pages/_document.js
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    title: 'Philip King - Blog and Portfolio',
+    description: 'Created using Nextra, Next.js, and Vercel',
+    image: ''
   }
 
   return (
@@ -16,11 +16,6 @@ export default function Document() {
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
       </Head>
       <body>
         <Main />
--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> © Philip King.
       <style jsx>{`
         a {
           float: right;
@@ -17,5 +16,6 @@ export default {
         }
       `}</style>
     </small>
-  )
+  ),
+  darkMode: true
 }
